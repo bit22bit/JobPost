67ac4eebf68f637adce6ecd9a8f830301d77726d
harishmashaik <harishmashaikk@gmail.com>
2020-11-30 21:41:20 -0500

    changes made in the gradle files and main activity_main.xml

--- app/src/main/java/com/example/jobpost/MainActivity.java
+++ app/src/main/java/com/example/jobpost/MainActivity.java
@@ -2,13 +2,30 @@ package com.example.jobpost;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button signup;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        signup=(Button)findViewById(R.id.signup);
+
+
+
     }
+    @Override
+    public void onClick(View v){
+        switch (v.getId()){
+            case R.id.signup:startActivity(new Intent(this,Signup.class));
+        }
+    }
+
 }
